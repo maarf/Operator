d9aeda16f06d6abcfd1670b083569fcbb9a1078e
Martins Spilners <martins@ctrld.ch>
2018-12-03 14:09:01 +0200

    Add Interpreter framework

---
+++ Interpreter/Interpreter.h
@@ -0,0 +1,19 @@
+//
+//  Interpreter.h
+//  Interpreter
+//
+//  Created by Martins on 03/12/2018.
+//  Copyright © 2018 Good Gets Better. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Interpreter.
+FOUNDATION_EXPORT double InterpreterVersionNumber;
+
+//! Project version string for Interpreter.
+FOUNDATION_EXPORT const unsigned char InterpreterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Interpreter/PublicHeader.h>
+
+
---
+++ InterpreterTests/InterpreterTests.swift
@@ -0,0 +1,34 @@
+//
+//  InterpreterTests.swift
+//  InterpreterTests
+//
+//  Created by Martins on 03/12/2018.
+//  Copyright © 2018 Good Gets Better. All rights reserved.
+//
+
+import XCTest
+@testable import Interpreter
+
+class InterpreterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
