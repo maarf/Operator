4c1eba5141673b4301ed5717fadec14f6b3f5f6b
Martins Spilners <martins@ctrld.ch>
2018-12-03 19:57:14 +0200

    Test connection to example router

--- Operator/MainController.swift
+++ Operator/MainController.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2018 Good Gets Better. All rights reserved.
 //
 
+import Interpreter
+import os.log
 import UIKit
 
 final class MainController {
@@ -22,6 +24,59 @@ final class MainController {
       top.navigationItem.leftBarButtonItem = split.displayModeButtonItem
       split.delegate = self
     }
+
+    logIn(username: "ios", password: "developer")
+  }
+
+  // MARK: - RouterOS client
+
+  private lazy var client: Client? = {
+    do {
+      return try Client(
+        hostname: "77.38.162.131",
+        port: 8728,
+        onReceive: handleResponse,
+        onError: handleError)
+    } catch {
+      os_log("Error when logging in: %@", error as CVarArg)
+      return nil
+    }
+  }()
+
+  private func logIn(username: String, password: String) {
+    do {
+      try client?.send(sentences: [Sentence(words: [
+        .command("login"),
+        .attribute(key: "name", value: username),
+        .attribute(key: "password", value: password),
+        .empty,
+      ])])
+    } catch {
+      os_log("Error when logging in: %@", error as CVarArg)
+    }
+  }
+
+  private func printStatus() {
+    do {
+      try client?.send(sentences: [Sentence(words: [
+        .command("interface/print"),
+        .attribute(key: "stats", value: nil),
+        .empty,
+      ])])
+    } catch {
+      os_log("Error when printing status: %@", error as CVarArg)
+    }
+  }
+
+  private func handleResponse(_ sentences: [Sentence]) {
+    os_log("Received: %@", sentences)
+    if sentences.first?.words.first == .reply("done") {
+      printStatus()
+    }
+  }
+
+  private func handleError(_ error: Error) {
+    os_log("Error: %@", type: .error, error as CVarArg)
   }
 }
 
